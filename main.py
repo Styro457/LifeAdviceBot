667904268314c0f93e72c76a4bf36bb8dce42c1d
Styro457 <raduursache6@gmail.com>
2021-12-05 00:57:17 +0200

    Created twitter bot that posts advice at a set interval

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
+import tweepy
 import ai
-import imageGen
+import json
+import time
 
-imageGen.generate_image(ai.generate_advice())
+settingsFile = open("twitter-settings.json")
+settings = json.load(settingsFile)
+
+print("Starting...")
+
+client = tweepy.Client(
+    consumer_key=settings['api_key'],
+    consumer_secret=settings['api_key_secret'],
+    access_token=settings['access_token'],
+    access_token_secret=settings['access_token_secret'],
+    wait_on_rate_limit=True
+)
+
+while True:
+    print(str(settings['mins_between_tweets']) + " minutes left until the next tweet.")
+    time.sleep(settings['mins_between_tweets']*60)
+    print("Generating advice...")
+    advice = ai.generate_advice()
+    print("Tweeting...")
+    client.create_tweet(text=advice)
+    print("Successfully Tweeted: " + advice)
