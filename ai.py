197b50737bde715a21518a925056054d2a5d13f5
Styro457 <raduursache6@gmail.com>
2021-12-05 01:33:14 +0200

    Merged ai-settings.json and twitter-settings.json into settings.json

--- ai.py
+++ ai.py
@@ -1,27 +1,37 @@
 import openai
-from pathlib import Path
-import json
 
-settingsFile = open("ai-settings.json")
-settings = json.load(settingsFile)
 
-openai.organization = settings["openai_org"]
-openai.api_key = settings["openai_key"]
+class AI:
 
-prompt = Path('prompt.txt').read_text()
+    def __init__(
+            self,
+            organization, api_key,
+            prompt, temperature, max_tokens, presence_penalty, frequency_penalty,
+            blacklisted_words):
 
-def generate_advice():
-    response = openai.Completion.create(
-        engine="davinci",
-        prompt=prompt,
-        temperature=settings["temperature"],
-        max_tokens=settings["max_tokens"],
-        stop="\n",
-        presence_penalty=settings["presence_penalty"],
-        frequency_penalty=settings["frequency_penalty"]
-    )
-    advice = response.get("choices")[0].get("text")
-    for blacklistedWord in settings["blacklistedWords"]:
-        if blacklistedWord in advice.lower():
-            return generate_advice()
-    return advice
+        openai.organization = organization
+        openai.api_key = api_key
+
+        self.prompt = prompt
+        self.temperature = temperature
+        self.max_tokens = max_tokens
+        self.presence_penalty = presence_penalty
+        self.frequency_penalty = frequency_penalty
+        self.blacklisted_words = blacklisted_words
+
+
+    def generate_advice(self):
+        response = openai.Completion.create(
+            engine="davinci",
+            prompt=self.prompt,
+            temperature=self.temperature,
+            max_tokens=self.max_tokens,
+            stop="\n",
+            presence_penalty=self.presence_penalty,
+            frequency_penalty=self.frequency_penalty
+        )
+        advice = response.get("choices")[0].get("text")
+        for blacklistedWord in self.blacklisted_words:
+            if blacklistedWord in advice.lower():
+                return self.generate_advice()
+        return advice
--- main.py
+++ main.py
@@ -1,30 +1,44 @@
-import tweepy
-import ai
+from pathlib import Path
+
 import json
 import time
+import tweepy
 
-settingsFile = open("twitter-settings.json")
+import ai
+
+settingsFile = open("settings.json")
 settings = json.load(settingsFile)
+
+adviceAI = ai.AI(
+    organization=settings['openai_organization'],
+    api_key=settings['openai_api_key'],
+    prompt=Path('prompt.txt').read_text(),
+    temperature=settings['ai_temperature'],
+    max_tokens=settings['ai_max_tokens'],
+    presence_penalty=settings['ai_presence_penalty'],
+    frequency_penalty=settings['ai_frequency_penalty'],
+    blacklisted_words=settings['blacklisted_words']
+)
 
 print("Starting...")
 
 client = tweepy.Client(
-    consumer_key=settings['api_key'],
-    consumer_secret=settings['api_key_secret'],
-    access_token=settings['access_token'],
-    access_token_secret=settings['access_token_secret'],
+    consumer_key=settings['twitter_api_key'],
+    consumer_secret=settings['twitter_api_key_secret'],
+    access_token=settings['twitter_access_token'],
+    access_token_secret=settings['twitter_access_token_secret'],
     wait_on_rate_limit=True
 )
 
 minutesLeft = settings['mins_between_tweets']
 
 while True:
-    for i in range(0, settings['mins_between_tweets']):
+    while minutesLeft > 0:
         print(str(minutesLeft) + " minutes left until the next tweet")
         minutesLeft -= 1
         time.sleep(60)
     print("Generating advice...")
-    advice = ai.generate_advice()
+    advice = adviceAI.generate_advice()
     print("Tweeting...")
     client.create_tweet(text=advice)
     print("Successfully Tweeted: " + advice)
